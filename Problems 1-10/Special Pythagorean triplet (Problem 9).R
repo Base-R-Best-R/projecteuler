7891e2d4d592cbf8a9615d66d9fdd16af9462cf0
StackOverthink <56105976+StackOverthink@users.noreply.github.com>
2020-01-13 13:56:25 +0100

    Update Special Pythagorean triplet (Problem 9).R

--- Problems 1-10/Special Pythagorean triplet (Problem 9).R
+++ Problems 1-10/Special Pythagorean triplet (Problem 9).R
@@ -7,6 +7,9 @@ library(rbenchmark)
 library(Rcpp)
 cppFunction("int product_abc(int x) {
   for(int a = 1; a <= x; a++){
+   if(a >= x){
+    return 0;
+   }
    for(int b = 1; b <= x; b++){
     for(int c = 1; c <= x; c++){
      if((a + b + c == x) && (a*a + b*b == c*c)){
